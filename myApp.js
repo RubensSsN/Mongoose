04f5fe3f56c7fd3509489be00267cb80b213ba03
RubensSamuel <contatorubeno@gmail.com>
2022-07-20 00:04:49 -0300

    Teste02

--- myApp.js
+++ myApp.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
-
+var mongoose = require('mongoose');
+const mySecret = process.env['MONGO_URI']
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
